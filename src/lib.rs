2b1fd290f3fb3c6c2a9e41a8613a0017c3f4bce4
Yusuf Simonson <simonson@gmail.com>
2014-11-03 20:42:28 -0500

    Data structures

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
-#[test]
-fn it_works() {
+extern crate serialize;
+
+use serialize::json;
+
+pub enum Id {
+    StringBased(String),
+    NumberBased(f64)
+}
+
+pub enum Parameters {
+    Positional(json::JsonList),
+    Named(json::JsonObject)
+}
+
+pub struct Request {
+    id: Option<Id>,
+    method: String,
+    params: Parameters
+}
+
+pub enum Response {
+    Normal(NormalResponse),
+    Error(ErrorResponse)
+}
+
+pub struct NormalResponse {
+    id: Id,
+    result: json::Json
+}
+
+pub struct ErrorResponse {
+    id: Option<Id>,
+    code: int,
+    message: String,
+    data: json::Json
 }
