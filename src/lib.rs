9e444e5f300d68f420296d70aea957fea77943a5
Yusuf Simonson <simonson@gmail.com>
2014-11-17 20:14:04 -0500

    Fixed warnngs

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,7 @@ extern crate iron;
 
 use serialize::json::{ToJson, Json, JsonList, JsonObject, Object, Null, encode, from_str};
 use std::collections::TreeMap;
-use std::io::net::ip::{IpAddr, Ipv4Addr};
-use iron::{status, Iron, IronResult};
+use iron::{status, IronResult};
 use iron::Request as IronRequest;
 use iron::Response as IronResponse;
 use std::str;
@@ -34,9 +33,9 @@ pub enum Parameters {
 }
 
 pub struct Request {
-    id: Option<Id>,
-    method: string::String,
-    params: Parameters
+    pub id: Option<Id>,
+    pub method: string::String,
+    pub params: Parameters
 }
 
 impl Request {
@@ -104,38 +103,34 @@ impl ErrorResponse {
         }
     }
 
-    fn newParseError() -> ErrorResponse {
+    fn new_parse_error() -> ErrorResponse {
         ErrorResponse::new(None, -32700, "Parse error".to_string(), Null)
     }
 
-    fn newInvalidRequest(data: Json) -> ErrorResponse {
+    fn new_invalid_request(data: Json) -> ErrorResponse {
         ErrorResponse::new(None, -32600, "Invalid Request".to_string(), data)
     }
 
-    pub fn newMethodNotFound(id: Id, data: Json) -> ErrorResponse {
+    pub fn new_method_not_found(id: Id, data: Json) -> ErrorResponse {
         ErrorResponse::new(Some(id), -32601, "Method not found".to_string(), data)
     }
 
-    pub fn newInvalidParams(id: Id, data: Json) -> ErrorResponse {
+    pub fn new_invalid_params(id: Id, data: Json) -> ErrorResponse {
         ErrorResponse::new(Some(id), -32602, "Invalid params".to_string(), data)
-    }
-
-    fn newInternalError(id: Id, data: Json) -> ErrorResponse {
-        ErrorResponse::new(Some(id), -32603, "Internal error".to_string(), data)
     }
 }
 
 impl ToJson for ErrorResponse {
     fn to_json(&self) -> Json {
-        let mut errorObj: TreeMap<string::String, Json> = TreeMap::new();
-        errorObj.insert("code".to_string(), self.code.to_json());
-        errorObj.insert("message".to_string(), self.message.to_json());
-        errorObj.insert("data".to_string(), self.data.to_json());
+        let mut error_obj: TreeMap<string::String, Json> = TreeMap::new();
+        error_obj.insert("code".to_string(), self.code.to_json());
+        error_obj.insert("message".to_string(), self.message.to_json());
+        error_obj.insert("data".to_string(), self.data.to_json());
 
         let mut obj: TreeMap<string::String, Json> = TreeMap::new();
         obj.insert("jsonrpc".to_string(), "2.0".to_string().to_json());
         obj.insert("id".to_string(), self.id.to_json());
-        obj.insert("error".to_string(), errorObj.to_json());
+        obj.insert("error".to_string(), error_obj.to_json());
 
         Object(obj)
     }
@@ -143,7 +138,7 @@ impl ToJson for ErrorResponse {
 
 pub struct Server {
     request_sender: Sender<(Vec<Request>, Sender<Vec<Response>>)>,
-    requests: Receiver<(Vec<Request>, Sender<Vec<Response>>)>
+    pub requests: Receiver<(Vec<Request>, Sender<Vec<Response>>)>
 }
 
 impl Server {
@@ -157,13 +152,13 @@ impl Server {
     }
 
     fn parse_json_request(&self, req: TreeMap<string::String, Json>) -> Result<Request, string::String> {
-        let version = req.find(&"jsonrpc".to_string());
+        let version = req.get(&"jsonrpc".to_string());
 
         if !version.is_some() || version.unwrap() != &Json::String("2.0".to_string()) {
             return Err("Invalid version specified".to_string());
         }
 
-        let method = match req.find(&"method".to_string()) {
+        let method = match req.get(&"method".to_string()) {
             Some(&Json::String(ref s)) => Some(s),
             _ => None
         };
@@ -172,7 +167,7 @@ impl Server {
             return Err("Invalid method specified".to_string());
         }
 
-        let params = match req.find(&"params".to_string()) {
+        let params = match req.get(&"params".to_string()) {
             Some(&Json::Object(ref o)) => Some(Parameters::Named(o.clone())),
             Some(&Json::List(ref l)) => Some(Parameters::Positional(l.clone())),
             _ => None
@@ -182,7 +177,7 @@ impl Server {
             return Err("Invalid params specified".to_string());
         }
 
-        let (id, is_invalid_id) = match req.find(&"id".to_string()) {
+        let (id, is_invalid_id) = match req.get(&"id".to_string()) {
             Some(&Json::String(ref s)) => (Some(Id::String(s.clone())), false),
             Some(&Json::I64(n)) => (Some(Id::I64(n)), false),
             Some(&Json::U64(n)) => (Some(Id::U64(n)), false),
@@ -207,10 +202,10 @@ impl Server {
             match json {
                 Json::Object(body) => match self.parse_json_request(body) {
                     Ok(request) => requests.push(request),
-                    Err(message) => responses.push(Response::Error(ErrorResponse::newInvalidRequest(Json::String(message))))
+                    Err(message) => responses.push(Response::Error(ErrorResponse::new_invalid_request(Json::String(message))))
                 },
 
-                _ => responses.push(Response::Error(ErrorResponse::newInvalidRequest(Json::String("Not a JSON object".to_string()))))
+                _ => responses.push(Response::Error(ErrorResponse::new_invalid_request(Json::String("Not a JSON object".to_string()))))
             }
         }
 
@@ -238,7 +233,7 @@ impl Server {
         let response_json = match str::from_utf8(req.body.as_slice()).and_then(|body| from_str(body).ok()) {
             Some(Json::Object(body)) => self.single_request(body),
             Some(Json::List(body)) => self.batch_request(body),
-            _ => Error(ErrorResponse::newParseError()).to_json()
+            _ => Error(ErrorResponse::new_parse_error()).to_json()
         };
 
         let response_str = encode(&response_json.to_json());
